360c15414b655b2662d91a82ced7bb1a1edc84c9
JuanNovais710 <jnovais710@outlook.com>
2025-08-07 23:50:28 -0300

    lists are now obtained from .xlsx files

--- estoque.py
+++ estoque.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import random
 from datetime import datetime, timedelta, time
-import os
 
+# Caminho da planilha a ser lida
 file_path = "C:\\Users\\user\\Desktop\\marcandoOTempo\\estoquePy\\lists\\NeoQuimica1.xlsx"
 
 # Lista de produtos em ordem alfabética
@@ -13,18 +13,19 @@ df = pd.read_excel(file_path)
 # Inicializa o array de produtos
 produtos = []
 
+# Altera o nome das colunas originais para: "id, produto, laboratorio, preçobase, subgrupo"
 df.rename(columns={
         'Cód. Interno': 'id',
-        'Produto': 'produto',
-        'Fornecedor': 'laboratorio',
-        'Preço base': 'preçoBase',
-        'Sub-Grupo': 'subGrupo'
+        'Produto': 'PRODUTO',
+        'Fornecedor': 'LABORATÓRIO',
+        'Preço base': 'PREÇO-BASE',
+        'Sub-Grupo': 'SUB-GRUPO'
     }, inplace=True)
 
 # Adicionar as novas colunas com valores aleatórios
 #df['id'] = ["100" + "".join([str(random.randint(0, 9)) for _ in range(4)]) for _ in range(len(df))]
 #df['preçoBase'] = np.random.uniform(10.0, 500.0, size=len(df))
-df['qtdVendas'] = np.random.randint(0, 999, size=len(df))
+df['QUANTIDADE DE VENDAS'] = np.random.randint(0, 500, size=len(df))
 
 # Gerar datas aleatórias para 2025
 start_date = datetime(2025, 1, 1)
@@ -39,13 +40,13 @@ def random_date_with_time(start, end):
     return random_day.replace(hour=random_hour, minute=random_minute, second=random_second)
 
 # A coluna `dataVenda` recebe o resultado completo da função, com data e hora
-df['dataVenda'] = [random_date_with_time(datetime(2025, 1, 1), datetime(2025, 12, 31)) for _ in range(len(df))]
+df['Data venda'] = [random_date_with_time(datetime(2025, 1, 1), datetime(2025, 12, 31)) for _ in range(len(df))]
 
 # Ordenar por nome do produto
-df_sorted = df.sort_values(by="produto")
+df_sorted = df.sort_values(by="PRODUTO")
 
 # Salvar como xlsx para Excel
-df_sorted.to_excel("estoque_ordenado.xlsx", index=False)
+df_sorted.to_excel("finalFIle.xlsx", index=False)
 
 # Imprimir o DataFrame ordenado no console
 print(df_sorted)
